dc4cfa5bd764b209c167a6ed4a460ece35439d92
gmesonero <gmesonero@gmail.com>
2023-05-22 14:52:05 -0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -18,6 +18,7 @@ form.addEventListener('submit', (e) => {
 
 function callAPI(city, country){
     const apiId = '41d1d7f5c2475b3a16167b30bc4f265c';
+    //const apiId = '65fbbe5c0a443c37334cec50bc6a4882';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`;
 
     fetch(url)
